fdb11d0304613305f28a7d65de655234ab1cc9ea
drozadilla <danielrozadilla@gmail.com>
2021-11-02 02:33:37 -0300

    Mapa con estilos. Con datos de prueba

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+let marcadores = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+  /*var filtro_t = document.getElementById('filtro_tipo');
+  filtro_t.addEventListener('change', filtrar);
+
+  var filtro_o = document.getElementById('filtro_oferta');
+  filtro_o.addEventListener('change', filtrar);
+
+  var filtro_p_max = document.getElementById('filtro_p_min');
+  filtro_p_max.addEventListener('change', filtrar);
+
+  var filtro_p_min = document.getElementById('filtro_p_max');
+  filtro_p_min.addEventListener('change', filtrar);
+*/
+
+
+  var aplicar_filtros = document.getElementById('aplicar_filtros');
+  aplicar_filtros.addEventListener('click', filtrar);
+
+}, false);
+
+
 function initMap() {
   getData();
 }
@@ -13,6 +36,7 @@ function cargarMapa(propiedades) {
     disableDefaultUI: true,
     zoomControl: true,
   });
+
   map.setOptions({
     styles: [
       /*{
@@ -34,96 +58,137 @@ function cargarMapa(propiedades) {
       },
     ]
   });
+  setMarkers(propiedades, map);
 
+}
+
+function setMarkers(propiedades, map) {
   for (let i = 0; i < propiedades.length; i++) {
-    let prop = propiedades[i];
-    if (prop[11] &&prop[12]) {    
+    let prop = propiedades[i];    
+    /*seleccion de imagen*/
+    if (prop[13] && prop[14]) {
       var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
       switch (prop[0]) {
         case "Búsqueda": {
-          image= "http://maps.google.com/mapfiles/ms/icons/yellow.png";
+          image = "https://maps.google.com/mapfiles/ms/icons/yellow.png";
           break;
         }
         case "Oportunidad": {
-          image="http://maps.google.com/mapfiles/ms/icons/green.png";
+          image = "https://maps.google.com/mapfiles/ms/icons/green.png";
           break;
         }
         case "Ingreso": {
-          image= "http://maps.google.com/mapfiles/ms/icons/blue.png";
+          image = "https://maps.google.com/mapfiles/ms/icons/blue.png";
           break;
         }
       };
-
+      /*--------*/
+      /*Formato de lat y long*/
       let latitud;
       let longitud;
 
-      if (prop[11]){
-        latitud=parseFloat(prop[11].replace(",", "."));
+      if (prop[13]) {
+        latitud = parseFloat(prop[13].replace(",", "."));
       }
-      if (prop[12]){
-        longitud=parseFloat(prop[12].replace(",", "."));
+      if (prop[14]) {
+        longitud = parseFloat(prop[14].replace(",", "."));
       }
-     
-    const marker = new google.maps.Marker({   
+      /*--------*/
 
-      position: {
-        lat: latitud,
-        lng: longitud
-      },
-      map,
-      icon: image,
-      title: prop[0]+": "+prop[4],
-      zIndex: i,
-      label: prop[1].toString(),
-    });
+      const marker = new google.maps.Marker({
+        position: {
+          lat: latitud,
+          lng: longitud
+        },
+        map,
+        icon: image,
+        //title: prop[0] + ": " + prop[3] + " " + prop[4] + " " + prop[5],
+        title: prop[0] + ": " + prop[1],
+        zIndex: i,
+        label: prop[1].toString(),
 
-    // Add a click listener for each marker, and set up the info window.
-    marker.addListener("click", () => {
-      //infoWindow.close();
-      //infoWindow.setContent(prop[4]);
-      //infoWindow.open(marker.getMap(), marker);
-      var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
-        keyboard: false
       });
-      myModal.toggle()      
-      let oferta = prop[0];
-      let tipo = prop[1];
-      let precio = prop[2];
-      let direccion = prop[3];
-      let observaciones = prop[4];
-      let link = prop[5];
-      let responsable = prop[8];
-      let email = prop[7];
-      let inmobiliaria = prop[9];
-      let telefono = prop[10];
 
-      document.getElementById("tipo").innerHTML = oferta + ": " + tipo;
-      document.getElementById("direccion").innerHTML = direccion;
-      document.getElementById("observaciones").innerHTML = observaciones;
-      document.getElementById("precio").innerHTML = precio;
-      document.getElementById("link").href = link;
-      if (link.length > 5) {        
-        document.getElementById("link").classList.remove("invisible");
-        document.getElementById("link").classList.add("visible");
-      } else {
-        document.getElementById("link").classList.remove("visible");
-        document.getElementById("link").classList.add("invisible");
-      }
-      document.getElementById("inmobiliaria").innerHTML = inmobiliaria;
-      document.getElementById("responsable").innerHTML = responsable;
-      document.getElementById("telefono").innerHTML = telefono;
-      document.getElementById("email").innerHTML = email;
-    });
-  }
+      // Add a click listener for each marker
+      marker.addListener("click", () => {
+        getFicha(prop)
+      });
+
+      marcadores.push({
+        'data': prop,
+        'marker': marker
+      });
+    }
   }
 }
 
+function filtrar() {  
+  let filtro_t = document.getElementById('filtro_tipo');
+  let filtro_o = document.getElementById('filtro_oferta');
+
+  marcadores.forEach(marcador => {
+    let cumple = true;
+    cumple = cumple && (filtro_t.value == "Todos" || marcador.data[1] == filtro_t.value);
+    cumple = cumple && (filtro_o.value == "Todos" || marcador.data[0] == filtro_o.value);
+    cumple = cumple && (filtro_p_min.value == "" || soloDigitos(marcador.data[2]) >= filtro_p_min.value);
+    cumple = cumple && (filtro_p_max.value == "" || soloDigitos(marcador.data[2]) <= filtro_p_max.value);
+    marcador.marker.setVisible(cumple);
+  });
+}
+
+
+function soloDigitos(texto) {
+  return parseInt(texto.replace(/[^\d,-]/g, ''));
+}
+
+/*Ficha detalle ----------------------------------*/
+function getFicha(prop) {
+  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+    keyboard: false
+  });
+  myModal.toggle();
+  let oferta = prop[0];
+  let tipo = prop[1];
+  let precio = prop[2];
+  let direccion = prop[3];
+  let calle = prop[4] != "Sin calle" ? prop[4] : "";
+  let numero = prop[5] != "Sin numero" ? prop[5] : "";
+  let observaciones = prop[6];
+  let link = prop[7];
+  let fecha = prop[8];
+  let email = prop[9];
+  let responsable = prop[10];
+  let inmobiliaria = prop[11];
+  let telefono = prop[12];
+  let lat = prop[13];
+  let long = prop[14];
+
+
+  document.getElementById("tipo").innerHTML = oferta + ": " + tipo;
+  document.getElementById("direccion").innerHTML = "<strong>Ubicación:</strong> " + direccion + " " + calle + " " + numero;
+  document.getElementById("observaciones").innerHTML = "<strong>Observaciones:</strong> " + observaciones;
+  document.getElementById("precio").innerHTML = "<strong>Precio:</strong> " + precio;
+  document.getElementById("link").href = link;
+  if (link.length > 5) {
+    document.getElementById("link").classList.remove("invisible");
+    document.getElementById("link").classList.add("visible");
+  } else {
+    document.getElementById("link").classList.remove("visible");
+    document.getElementById("link").classList.add("invisible");
+  }
+  document.getElementById("inmobiliaria").innerHTML = "<strong>Inmobiliaria:</strong> " + inmobiliaria;
+  document.getElementById("responsable").innerHTML = "<strong>Responsable:</strong> " + responsable;
+  document.getElementById("telefono").innerHTML = "<strong>Teléfono:</strong> " + telefono;
+  document.getElementById("email").innerHTML = "<strong>E-mail:</strong> " + email;
+}
+
 function getData() {
-  var url = "https://sheets.googleapis.com/v4/spreadsheets/1h5QpK4d5NAmDET20Pe7tAgDYcWjXzztUSV7Qq0ALIuk/values/Propiedades!A6:M?key=AIzaSyBb6E0QnyBTlhp-b9JyEyWsL9qAHhKcAiw";
+  /*Prueba*/ var url = "https://sheets.googleapis.com/v4/spreadsheets/1Z6ckEgXOxzM9tjS9p-w7YTqXjf9D6IF_PiJlP4j8KKU/values/Mapa!A6:O?key=AIzaSyBb6E0QnyBTlhp-b9JyEyWsL9qAHhKcAiw";
+  //var url = "https://sheets.googleapis.com/v4/spreadsheets/1h5QpK4d5NAmDET20Pe7tAgDYcWjXzztUSV7Qq0ALIuk/values/Mapa!A6:O?key=AIzaSyBb6E0QnyBTlhp-b9JyEyWsL9qAHhKcAiw";
+
   var datos = [];
   fetch(url).then(response => response.json())
-    .then(data => {
-      console.log(data);
+    .then(data => {    
       cargarMapa(data.values);
     });
 }
